f34ee792f2612d03a146ebfdaa2e02e2f7ed3e96
Vicente <165604764+mvr2004@users.noreply.github.com>
2025-01-30 01:42:42 +0000

    Add files via upload

--- BD2_Trabalhofinal/BD2_Trabalhofinal/settings.py
+++ BD2_Trabalhofinal/BD2_Trabalhofinal/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'projeto_BD2',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '5432',
     },
