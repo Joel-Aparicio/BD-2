87148195249c62b38aed800cd113968391064594
Miguel Da Silva <infotecjuniorr@gmail.com>
2025-02-08 16:11:09 +0000

    Update Utilizadores_postgreSQL.sql

--- Utilizadores_postgreSQL.sql
+++ Utilizadores_postgreSQL.sql
@@ -6,21 +6,13 @@ CREATE TABLE P_Utilizador (
     utilizador_id SERIAL PRIMARY KEY,       -- Chave primária com incremento automático
     nome VARCHAR(50) NOT NULL,              -- Nome do utilizador, obrigatório
     email VARCHAR(200) NOT NULL UNIQUE,     -- Email obrigatório e único
-    palavra_passe VARCHAR(200) NOT NULL,    -- Palavra-passe obrigatória
-    ser_admin BOOL NOT NULL DEFAULT FALSE,  -- Indica se é admin, com valor padrão FALSE
+    password VARCHAR(200) NOT NULL,    -- Palavra-passe obrigatória
+    is_staff BOOL NOT NULL DEFAULT FALSE,  -- Indica se é admin, com valor padrão FALSE
+	is_superuser BOOL NOT NULL DEFAULT FALSE,
     last_login TIMESTAMP,                   -- Data e hora do último login
     is_active BOOL NOT NULL DEFAULT TRUE    -- Indica se a conta está ativa
 );
 
--- Inserir Valores
-
--- Utilizador Normal
-INSERT INTO P_Utilizador (nome, email, palavra_passe) 
-VALUES ('Marco Vicente', 'marco.vicente@email.com', 'senha123');
-
--- Administrador
-INSERT INTO P_Utilizador (nome, email, palavra_passe, ser_admin) 
-VALUES ('Miguel Silva', 'miguel.silva@email.com', 'senha123', TRUE);
 
 -- Mostrar a tabela para verificar os dados
 SELECT * 
